130ef21322c4cbfad836728616695486dd65b175
haluskua <haluskua.one@gmail.com>
2020-06-03 22:45:26 +1200

    conplete small animator library challenge

--- src/js/animation.js
+++ src/js/animation.js
@@ -2,11 +2,34 @@ class Animator {
   constructor(selector) {
     this.selector = document.querySelector(selector);
   }
-  //pass in methods
-  fadeOut(time) {
-    this.selector.style.transition = `${time}s ease`;
-    this.selector.style.opacity = 0;
+  //pass in *fadeout methods
+  fadeOut(time, toggle = false) {
+    if (toggle && this.selector.classList.contains("fadeOut-active")) {
+      this.selector.style.opacity = 1;
+      this.selector.classList.remove("fadeOut-active");
+
+    } else {
+      this.selector.style.transition = `${time}s ease`;
+      this.selector.style.opacity = 0;
+      this.selector.classList.add("fadeOut-active");
+    }
   }
+
+  //////////////////////
+   //pass in *move methods
+   move(time, toggle = false, {x = 0, y = 0}) {
+    if (toggle && this.selector.classList.contains("move-active")) {
+      this.selector.style.transform = "translate(0px, 0px)";
+      this.selector.classList.remove("move-active");
+
+    } else {
+      this.selector.style.transition = `${time}s ease`;
+      this.selector.style.transform = `translate(${x}px, ${y}px)`;
+      this.selector.classList.add("move-active");
+    }
+  }
+
+  
 }
 
 // bringing classes from HTML into constructor
@@ -18,5 +41,6 @@ const intro = new Animator(".toggleFade");
 const button = document.querySelector(".toggleFade");
 
 button.addEventListener("click", () => {
-  intro.fadeOut(2);
+  intro.move(2, true, {x: 100});
+  intro.fadeOut(3, true);
 });
