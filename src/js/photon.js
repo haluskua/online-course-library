5e5b3acdfb3254a50d0a9bedbb4530bcfa189122
haluskua <haluskua.one@gmail.com>
2020-07-08 10:01:14 +1200

    fixed api address call to get numerous photos

--- src/js/photon.js
+++ src/js/photon.js
@@ -12,7 +12,7 @@ let searchValue;
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=1",
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
     {
       method: "GET",
       headers: {
@@ -23,6 +23,7 @@ async function curatedPhotos() {
   );
   const data = await dataFetch.json();
   data.photos.forEach((photo) => {
+      console.log(photo)
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `<img src=${photo.src.large}></img> 
