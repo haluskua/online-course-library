efd5cb77a814ba40f0285b3c10236881dc58c086
haluskua <haluskua.one@gmail.com>
2020-07-09 16:00:38 +1200

    added home link to title on photon page

--- src/js/photon.js
+++ src/js/photon.js
@@ -4,17 +4,23 @@ const auth = "563492ad6f91700001000001b812ffb7e89f43448454ef0c6e746aa9";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
 
 //search value variable:
-
+let page = 1;
 let searchValue;
+let fetchLink;
+let currentSearch;
 
 //EventListener
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+
+more.addEventListener('click', loadMore);
 
 function updateInput(e) {
   //   console.log(e.target.value);
@@ -49,15 +55,15 @@ function generatePictures(data) {
 
 //function that gets images
 async function curatedPhotos() {
-  const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=7&page=1");
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=7&page=1";
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(query) {
     clear();
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${query}+query&per_page=7&page=1`
-  );
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=7&page=1`
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
@@ -67,5 +73,16 @@ function clear() {
     searchInput.value= "";
 }
 
+
+async function loadMore(){
+    page++;
+    if(currentSearch){
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=7&page=${page}`
+    }else{
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=7&page=${page}`
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
 
 curatedPhotos();
